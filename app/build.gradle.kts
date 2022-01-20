fddf01af89f4a82a0325237bccedd92c812ec3ca
Malgeon <nyangsun@gmail.com>
2022-01-20 16:17:40 +0900

    add - 뷰를 위한 fragment 추가 및 navigation 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     kotlin("android")
     kotlin("kapt")
     id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-android")
 }
 
 android {
@@ -39,8 +41,14 @@ android {
 dependencies {
     implementation ("androidx.core:core-ktx:1.7.0")
     implementation ("androidx.appcompat:appcompat:1.4.1")
-    implementation ("com.google.android.material:material:1.4.0")
+    implementation ("com.google.android.material:material:1.5.0")
     implementation ("androidx.constraint's:constraint's:2.1.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    implementation ("androidx.fragment:fragment-ktx:1.4.0")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
+
     testImplementation ("junit:junit:4.+")
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
---
+++ app/src/main/java/com/example/libvlc_custom/ui/player1/Player1Fragment.kt
@@ -0,0 +1,19 @@
+package com.example.libvlc_custom.ui.player1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.libvlc_custom.R
+
+class Player1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_player1, container, false)
+    }
+}
