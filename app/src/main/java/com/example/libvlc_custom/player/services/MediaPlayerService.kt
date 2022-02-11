d0494f00a002bd6b95a875fdeb7e8c8ed37997c5
Malgeon <nyangsun@gmail.com>
2022-02-11 16:39:42 +0900

    remove - notification 제거

--- app/src/main/java/com/example/libvlc_custom/player/services/MediaPlayerService.kt
+++ app/src/main/java/com/example/libvlc_custom/player/services/MediaPlayerService.kt
@@ -1,14 +1,11 @@
 package com.example.libvlc_custom.player.services
 
-import android.app.Notification
-import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.media.AudioManager
 import android.net.Uri
-import android.os.Build
 import android.os.IBinder
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
@@ -17,7 +14,6 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.SurfaceView
 import android.widget.Toast
-import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.media.session.MediaButtonReceiver
 import com.example.libvlc_custom.R
@@ -55,7 +51,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
     private var audioManager: AudioManager? = null
     private var audioFocusChangeListener: WeakReference<AudioManager.OnAudioFocusChangeListener>? =
         null
-//    private var notificationManager: NotificationManager? = null
     private var stateBuilder: PlaybackStateCompat.Builder? = null
     private var mediaBitmap: Bitmap? = null
     private var defaultBitmap: Bitmap? = null
@@ -103,26 +98,20 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
         )
 
         binder = MediaPlayerServiceBinder(this)
-//        notificationManager = NotificationUtils.getNotificationManager(applicationContext)
         stateBuilder = PlaybackStateCompat.Builder()
             .setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE or PlaybackStateCompat.ACTION_SEEK_TO)
             .setState(PlaybackStateCompat.STATE_PAUSED, 0L, 1f)
 
         createMediaSession()
-//        createNotificationChannel()
-
         player?.callback = this
 
         libVlc?.let {
             rendererItemObservable = RendererItemObservable(it)
             rendererItemObservable?.start()
         }
-
-        Log.e("MediaPlayerService", "onCreate")
     }
 
     override fun onDestroy() {
-        Log.e("MediaPlayerService", "onDestroy")
         stopForeground(true)
         Dialog.setCallbacks(libVlc, null)
         player?.release()
@@ -186,21 +175,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
                     setVolume(50)
             }
         }
-//
-//    private fun createNotificationChannel() {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-//            return
-//        }
-//
-//        NotificationUtils.makeNotificationChannel(
-//            MediaPlayerServiceChannelId,
-//            MediaPlayerServiceChannelName,
-//            notificationManager,
-//            mEnableSound = false,
-//            mEnableLights = false,
-//            mEnableVibration = false
-//        )
-//    }
 
     private fun onPerformanceWarningDialog(questionDialog: Dialog.QuestionDialog) {
         // Let the user know casting will eat their battery.
@@ -241,11 +215,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
         mediaSession?.setMetadata(
             getMediaMetadata(mediaBitmap)
         )
-
-//        startForeground(
-//            MediaPlayerServiceNotificationId,
-//            buildPlaybackNotification()
-//        )
     }
 
     private fun getMediaMetadata(bitmap: Bitmap?): MediaMetadataCompat {
@@ -270,90 +239,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
         mediaSession?.setPlaybackState(stateBuilder?.build())
     }
 
-//    private fun updateNotification() {
-//        if (player?.selectedRendererItem == null) {
-//            return
-//        }
-//
-//        notificationManager?.notify(
-//            MediaPlayerService.MediaPlayerServiceNotificationId,
-//            buildPlaybackNotification()
-//        )
-//    }
-//
-//    private fun getPauseAction(context: Context): NotificationCompat.Action {
-//        return NotificationCompat.Action(
-//            R.drawable.ic_pause_black_36dp,
-//            "Pause",
-//            MediaButtonReceiver.buildMediaButtonPendingIntent(
-//                context,
-//                PlaybackStateCompat.ACTION_PAUSE
-//            )
-//        )
-//    }
-//
-//    private fun getPlayAction(context: Context): NotificationCompat.Action {
-//        return NotificationCompat.Action(
-//            R.drawable.ic_play_arrow_black_36dp,
-//            "Play",
-//            MediaButtonReceiver.buildMediaButtonPendingIntent(
-//                context,
-//                PlaybackStateCompat.ACTION_PLAY
-//            )
-//        )
-//    }
-//
-//    private fun getStopAction(context: Context): NotificationCompat.Action {
-//        return NotificationCompat.Action(
-//            R.drawable.ic_clear_black_36dp,
-//            "Stop",
-//            MediaButtonReceiver.buildMediaButtonPendingIntent(
-//                context,
-//                PlaybackStateCompat.ACTION_STOP
-//            )
-//        )
-//    }
-//
-//    private fun buildPlaybackNotification(): Notification {
-//        val context = applicationContext
-//
-//        val title = player
-//            ?.media
-//            ?.uri
-//            ?.lastPathSegment
-//            ?: ""
-//
-//        val builder = NotificationCompat.Builder(
-//            context,
-//            MediaPlayerService.MediaPlayerServiceChannelId
-//        )
-//
-//        builder.setCategory(NotificationCompat.CATEGORY_SERVICE)
-//            .setSmallIcon(R.drawable.ic_play_arrow_black_36dp)
-//            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-//            .setLargeIcon(mediaBitmap)
-//            .setContentTitle(title)
-//            .setContentText(null)
-//            .setTicker(title)
-//            .setAutoCancel(false)
-//            .setOngoing(true)
-//
-//        if (player?.isPlaying == true) {
-//            builder.addAction(getPauseAction(context))
-//        } else {
-//            builder.addAction(getPlayAction(context))
-//        }
-//
-//        builder.addAction(getStopAction(context))
-//
-//        builder.setStyle(
-//            androidx.media.app.NotificationCompat.MediaStyle()
-//                .setMediaSession(mediaSession!!.sessionToken)
-//                .setShowActionsInCompactView(0, 1)
-//        )
-//        return builder.build()
-//    }
-
     private fun gainAudioFocus() {
         // Only gain audio focus when playing locally.
         if (player?.selectedRendererItem != null) {
@@ -370,7 +255,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
         lastUpdateTime = 0L
 
         updatePlaybackState()
-//        updateNotification()
         callback?.onPlayerOpening()
     }
 
@@ -381,7 +265,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
 
     override fun onPlayerPlaying() {
         updatePlaybackState()
-//        updateNotification()
 
         mediaSession?.isActive = true
         if (player?.selectedRendererItem != null) {
@@ -392,13 +275,11 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
 
     override fun onPlayerPaused() {
         updatePlaybackState()
-//        updateNotification()
         callback?.onPlayerPaused()
     }
 
     override fun onPlayerStopped() {
         updatePlaybackState()
-//        updateNotification()
         mediaSession?.isActive = false
         stopForeground(true)
         callback?.onPlayerStopped()
@@ -406,14 +287,12 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
 
     override fun onPlayerEndReached() {
         updatePlaybackState()
-//        updateNotification()
         mediaSession?.isActive = false
         callback?.onPlayerEndReached()
     }
 
     override fun onPlayerError() {
         updatePlaybackState()
-//        updateNotification()
         mediaSession?.isActive = false
         callback?.onPlayerError()
     }
@@ -502,7 +381,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
             }
             return true
         }
-
     }
 
     private fun setMediaBitmap(mediaUri: Uri): Bitmap {
@@ -519,10 +397,8 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
         if (mediaBitmap == null) {
             mediaBitmap = defaultBitmap
         }
-
         return mediaBitmap!!
     }
-
 
     fun setMedia(
         context: Context?,
@@ -588,9 +464,6 @@ class MediaPlayerService : Service(), MediaPlayer.Callback, Dialog.Callbacks {
         private const val Tag = "MediaPlayerService"
         const val RendererClearedAction = "action.rendererclearedaction"
         const val RendererSelectionAction = "action.rendererselectionaction"
-        private const val MediaPlayerServiceChannelName = "Media Player Service"
-        private const val MediaPlayerServiceChannelId = "channel.mediaplayerservice"
         private const val SimpleVlcSessionTag = "tag.libvlcsession"
-        private const val MediaPlayerServiceNotificationId = 1
     }
 }
